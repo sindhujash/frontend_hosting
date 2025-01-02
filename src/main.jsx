425a14f3ee7267f3e50063fb5d9a9b6b67054388
sindhujash <sindhujash27@gmail.com>
2025-01-02 22:39:17 +0530

    txtfile

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 //import Form from './Form.jsx'
-import Sindhuja from './Sindhuja.jsx'
-
+// import Sindhuja from './Sindhuja.jsx'
+import Appparam from './Appparams'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-     {/* <Form/>  */}
-    <Sindhuja />
+    {/* <App />
+      <Form/>  
+    <Sindhuja /> */}
+      <Appparam />
   </StrictMode>,
 )
